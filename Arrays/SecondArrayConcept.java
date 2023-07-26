90b48ff3068c67de3263bae14d4db2aae6de7704
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-07-26 07:52:41 +0545

    arrays

---
+++ Arrays/SecondArrayConcept.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class SecondArrayConcept {
+    public static void main(String[] args) {
+        //create array and initialize it
+        int a[] = {10,20,30,40,50};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+
+//        int c[] = new int[0];
+//        c[0] =10;
+  //      System.out.println(c[0]); ArrayIndexOutOfBoundsException
+
+        //finding the length of an array
+        System.out.println(a.length);
+        System.out.println(a.length-1);
+        System.out.println(a.length*2);
+        System.out.println(a.length);
+    }
+}
