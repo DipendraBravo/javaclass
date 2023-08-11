5df235565d0815df22463226868e6ea76bfcb178
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-08-11 07:43:29 +0545

    linkedlist:set() and remove() add()

--- Collections/LinkedListPractice.java
+++ Collections/LinkedListPractice.java
@@ -13,5 +13,15 @@ public class LinkedListPractice {
         linkedList.add(10);
         linkedList.add(30);
         System.out.println(linkedList);
+
+        linkedList.remove(0);
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.set(4,400);
+        System.out.println(linkedList);
+
+        linkedList.add(5,500);
+        System.out.println(linkedList);
     }
 }
