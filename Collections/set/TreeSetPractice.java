a58117c3180cb49123651940b3bbb0c3b714ed34
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-08-12 12:58:23 +0545

    null is not accepeted in treeset

--- Collections/set/TreeSetPractice.java
+++ Collections/set/TreeSetPractice.java
@@ -5,7 +5,7 @@ import java.util.TreeSet;
 public class TreeSetPractice {
     public static void main(String[] args) {
 
-        //Natural Sorting order //No duplicate value
+        //Natural Sorting order //No duplicate value //null not allowed
         TreeSet<String> treeSet = new TreeSet<>();
         treeSet.add("A");
         treeSet.add("C");
@@ -18,5 +18,8 @@ public class TreeSetPractice {
         treeSet.add("F");
         treeSet.add("H");
         System.out.println(treeSet);
+
+        treeSet.add(null);
+        System.out.println(treeSet); //throws java.lang.NullPointerException
     }
 }
