7d580ccbe6345b5c49c24ea80e47bfe35e74e6f9
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-08-12 12:11:12 +0545

    vector method addelement, clear, firstelement last element

--- Collections/VectorPractice.java
+++ Collections/VectorPractice.java
@@ -6,11 +6,24 @@ public class VectorPractice {
     public static void main(String[] args) {
 
         Vector<String> vector = new Vector<>();
-        vector.add("NameOne");
-        vector.add("NameTwo");
-        vector.add("NameThree");
-        vector.add("NameFour");
-        vector.add("NameFive");
+//        vector.add("NameOne");
+//        vector.add("NameTwo");
+//        vector.add("NameThree");
+//        vector.add("NameFour");
+//        vector.add("NameFive");
+        //addElement function()
+            vector.addElement("NameOne");
+            vector.addElement("NameTwo");
+            vector.addElement("NameThree");
+            vector.addElement("NameFour");
+            vector.addElement("NameFive");
+            vector.addElement("NameFive");
+            System.out.println(vector);
+        //fetching first element and last element
+        System.out.println(vector.lastElement());
+        System.out.println(vector.firstElement());
+            //helps to clear the vector
+            vector.clear();
         System.out.println(vector);
 
         //passing object in the constructor
@@ -18,6 +31,8 @@ public class VectorPractice {
         v.add("NameSix");
         v.add("NameSeven");
         System.out.println(v);
+
+
 
     }
 }
