175ea8d5f098f00f52304de253c8a994e6676180
Dipendra Tamang <dipendra.tamang9@gmail.com>
2023-08-16 07:31:41 +0545

    java8feature functional interface

---
+++ java8features/FunctionalInterfacePractice.java
@@ -0,0 +1,23 @@
+package java8features;
+
+@FunctionalInterface
+interface Properties{
+   void add(int a, int b);
+}
+public class FunctionalInterfacePractice implements Properties {
+    public static void main(String[] args) {
+        FunctionalInterfacePractice functionalInterfacePractice  = new FunctionalInterfacePractice();
+        functionalInterfacePractice.add(12,12);
+
+        //polymorphsim
+        Properties properties = new FunctionalInterfacePractice();
+        properties.add(5,6);
+
+    }
+
+    @Override
+    public void add(int a, int b) {
+        System.out.println(a+b);
+
+    }
+}
